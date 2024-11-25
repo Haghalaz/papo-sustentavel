419424c7140180455983f85be9532b16aee6a52d
Luiz Coelho <107281502+Haghalaz@users.noreply.github.com>
2024-11-25 20:09:51 +0000

    refactor: Adjust layout

--- src/components/organism/chatbot.tsx
+++ src/components/organism/chatbot.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import sendMessage from "../../utils/Gemini.ts";
 
 import Message from "../atoms/message.tsx";
@@ -6,45 +6,56 @@ import Header from "../atoms/header.tsx";
 import SendMessage from "../molecules/send-message.tsx";
 
 const ChatInterface: React.FC = () => {
-    const [messages, setMessages] = useState<{ role: "user" | "bot"; text: string }[]>([]);
-    const [isloadingMessage, setIsloadingMessage] = useState(false)
-    const [input, setInput] = useState("");
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const [messages, setMessages] = useState<{ role: "user" | "bot"; text: string }[]>([]);
+  const [isloadingMessage, setIsloadingMessage] = useState(false)
+  const [input, setInput] = useState("");
 
-    const handleSend = async () => {
-        if (!input.trim()) return;
+  const handleSend = async () => {
+    if (!input.trim()) return;
 
-        setMessages((prev) => [...prev, { role: "user", text: input }]);
-        setIsloadingMessage(true)
-        setInput("");
+    setMessages((prev) => [...prev, { role: "user", text: input }]);
+    setIsloadingMessage(true)
+    setInput("");
 
-        try {
-            const botResponse = await sendMessage(input);
-            setMessages((prev) => [...prev, { role: "bot", text: botResponse }]);
-        } catch (error) {
-            setMessages((prev) => [...prev, { role: "bot", text: "Ocorreu um erro. Recarregue a página ou tente novamente mais tarde." }]);
-            console.error(error);
-        } finally {
-            setIsloadingMessage(false)
-        }
-    };
+    try {
+      const botResponse = await sendMessage(input);
+      setMessages((prev) => [...prev, { role: "bot", text: botResponse }]);
+    } catch (error) {
+      setMessages((prev) => [...prev, { role: "bot", text: "Ocorreu um erro. Recarregue a página ou tente novamente mais tarde." }]);
+      console.error(error);
+    } finally {
+      setIsloadingMessage(false)
+    }
+  };
 
-    return (
-      <main className="bg-[#3C3C3C] h-screen flex justify-between flex-col">
-        <Header />
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
-          <div className="space-y-12 px-12 py-6 grow overflow-y-auto md:px-24">
-            {messages.map(({text, role}, index) => (
-              <Message key={index} text={text} role={role}/>
-            ))}
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
-            {isloadingMessage && (
-              <Message className="animate-pulse" text={"Digitando..."} role={"bot"}/>
-            )}
-          </div>
+  return (
+    <main className="bg-[#3C3C3C] h-screen flex justify-between flex-col">
+      <Header />
 
-        <SendMessage input={input} setInput={setInput} loading={isloadingMessage} onSend={handleSend}/>
-      </main>
-    );
+      <div className="space-y-12 px-12 py-6 grow overflow-y-auto md:px-24">
+        {messages.map(({text, role}, index) => (
+          <Message key={index} text={text} role={role}/>
+        ))}
+
+        {isloadingMessage && (
+          <Message className="animate-pulse" text={"Digitando..."} role={"bot"}/>
+        )}
+
+        <span ref={messagesEndRef}/>
+      </div>
+
+      <SendMessage input={input} setInput={setInput} loading={isloadingMessage} onSend={handleSend}/>
+    </main>
+  );
 };
 
 export default ChatInterface;
